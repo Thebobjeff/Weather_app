8b022c7be44007567a0173168f8b1870db67fcc9
Devon2575 <94057514+Thebobjeff@users.noreply.github.com>
2024-04-24 15:48:05 -0400

    fix 2.0

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -7,22 +7,19 @@ export const Homepage = () => {
   const size = useSize();
   const [forcastlen, setForcastlen] = useState(7);
 
-  useEffect(
-    () => {
-      if (size && size <= 320) {
-        setForcastlen(3);
-      } else if (size && size > 320 && size <= 430) {
-        setForcastlen(4);
-      } else if (size && size > 430 && size <= 508) {
-        setForcastlen(5);
-      } else if (size && size > 508 && size <= 768) {
-        setForcastlen(5);
-      } else {
-        setForcastlen(7);
-      }
-    },
-    { forcastlen }
-  );
+  useEffect(() => {
+    if (size && size <= 320) {
+      setForcastlen(3);
+    } else if (size && size > 320 && size <= 430) {
+      setForcastlen(4);
+    } else if (size && size > 430 && size <= 508) {
+      setForcastlen(5);
+    } else if (size && size > 508 && size <= 768) {
+      setForcastlen(5);
+    } else {
+      setForcastlen(7);
+    }
+  }, [size, forcastlen]);
   const data = useFetch(forcastlen);
 
   return (
